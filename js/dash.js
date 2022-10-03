711fd39d4c50cb38bf0a8372f38a4691decf5c15
echezonadev <you@example.com>
2022-10-03 22:16:39 +1100

    goblog

--- js/dash.js
+++ js/dash.js
@@ -1,4 +1,58 @@
+function views() { 
+    let viewalbum=document.getElementById("viewalbum");  
+    let mainalbum=document.getElementById("mainalbum");  
+    let goalbum=document.getElementById("goalbum");  
+    goalbum.addEventListener("click",()=>{ 
+        goalbum.style.display="none"
+        viewalbum.style.display="block"
+        mainalbum.style.height="0px"
+        mainalbum.style.transition="1s"
+        mainalbum.style.margin="auto" 
+        mainalbum.style.padding="0px" 
+        mainalbum.style.overflow="scroll" 
+        mainalbum.style.border="none" 
+    })
+    viewalbum.addEventListener("click",()=>{
+        goalbum.style.display="block"
+        viewalbum.style.display="none"
+        mainalbum.style.height="500px"
+        mainalbum.style.transition="1s"
+        mainalbum.style.margin="auto" 
+        mainalbum.style.padding="4px" 
+        mainalbum.style.overflow="scroll" 
+        mainalbum.style.border="solid #e6e6e6 2px" 
+    })
+}
+views();
+function albumside() { 
+    let album=document.getElementById("album"); 
+    let albumdiv=document.getElementById("albumdiv"); 
+    let mudicdiverf=document.getElementById("mudicdiverf"); 
+    let muid=document.getElementById("muid"); 
+    album.addEventListener("click",()=>{
+        albumdiv.style.display="block"
+        muid.style.display="none"
+        mudicdiverf.style.backgroundColor="white" 
+        mudicdiverf.style.border="solid #e6e6e6 2px" 
+        mudicdiverf.style.color="black" 
+        album.style.backgroundColor="black" 
+        album.style.border="none" 
+        album.style.color="white" 
+    })   
+    mudicdiverf.addEventListener("click",()=>{
+        albumdiv.style.display="none"
+        muid.style.display="block"
+        mudicdiverf.style.backgroundColor="black" 
+        mudicdiverf.style.border="none" 
+        mudicdiverf.style.color="white" 
+        album.style.backgroundColor="white" 
+        album.style.border="solid #e6e6e6 2px " 
+        album.style.color=" black" 
+    })
+}
+albumside()
 
+function dashbod() {
 let drop=document.getElementById("drop"); 
 let droper=document.getElementById("dropshow");
 let allos=document.getElementById("allos");
@@ -43,3 +97,5 @@ dropesr.addEventListener("click",()=>{
 droper.addEventListener("click",()=>{
     drop.style.display = drop.style.display === "block"? "none": "block"  
 }); 
+}
+dashbod()
